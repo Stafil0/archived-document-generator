a26a5b2be2ce8b21bf9d8be7b4f43642a3772138
riftgetalone <fort.kto2@gmail.cm>
2019-07-24 14:17:18 +0400

    файлы проекта

---
+++ Layout/LayoutGenerator.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Layout
+{
+     class LayoutGenerator
+    {
+        private Document.DocumentLayout document;
+        private Document.DocumentTemplate documentTemplate;
+        private Random rand;
+        private Encoding encoding;
+        private CultureInfo dateCulture;
+
+        public LayoutGenerator(string jsonPath, Encoding encoding)
+        {
+            this.encoding = encoding;
+            document = InitDocumentLayout();
+            rand = new Random();
+            dateCulture = CultureInfo.InvariantCulture;
+            JsonParser.GetObject<Document.DocumentTemplate>(jsonPath, encoding, out documentTemplate);
+        }
+
+        public void GenerateLayout()
+        {
+            document.size = documentTemplate.size;
+            document.background = documentTemplate.background;
+            document.rotation = documentTemplate.rotation != null ? GetRandomIntInRange(documentTemplate.rotation) : 0;
+            document.shiftX = GetRandomIntInRange(documentTemplate.shiftX);
+            document.shiftY = GetRandomIntInRange(documentTemplate.shiftY);
+
+            foreach (Document.DocumentTemplate.LayoutTemplate layoutTemplate in documentTemplate.layout)
+            {
+                Document.DocumentLayout.Layout layout = new Document.DocumentLayout.Layout();
+                layout.type = layoutTemplate.type;
+                layout.location = layoutTemplate.location;
+
+                if (layout.type != Document.Type.image.ToString())
+                {
+                    if (layout.type == Document.Type.text.ToString())
+                    {
+                        string format = layoutTemplate.format ?? documentTemplate.textDefault.format;
+                        string value =
+                            (layoutTemplate.value ?? GetRandomValueFromFile(layoutTemplate.valuePath)) ??
+                            (documentTemplate.textDefault.value ?? GetRandomValueFromFile(documentTemplate.textDefault.valuePath));
+                        layout.value = String.Format(format, value);
+                    }
+                    else
+                    {
+                        string format = layoutTemplate.format ?? documentTemplate.dateDefault.format;
+                        string value = 
+                            (
+                                layoutTemplate.value ??
+                                GetRandomDateInRange(layoutTemplate.dateRange)
+                            ) ??
+                            (
+                                documentTemplate.dateDefault.value ??
+                                GetRandomDateInRange(documentTemplate.dateDefault.dateRange)
+                            );
+                        layout.value = String.Format(format, GetDateFromString(value));
+                    }
+
+                    layout.alignmentX = GetRandomStringFromArray(
+                        layoutTemplate.alignmentX ??
+                        (layout.type == Document.Type.text.ToString() ? 
+                            documentTemplate.textDefault.alignmentX : documentTemplate.dateDefault.alignmentY)
+                    );
+                    layout.alignmentY = GetRandomStringFromArray(
+                        layoutTemplate.alignmentY ??
+                        (layout.type == Document.Type.text.ToString() ? 
+                            documentTemplate.textDefault.alignmentY : documentTemplate.dateDefault.alignmentY)
+                    );
+                    layout.font = GetRandomFontFromResources(
+                        layoutTemplate.font ?? 
+                        (layout.type == Document.Type.text.ToString() ? 
+                            documentTemplate.textDefault.font : documentTemplate.dateDefault.font)
+                    );
+                    layout.rotation = GetRandomTextRotationInRange(
+                        layoutTemplate.rotation ??
+                        (layout.type == Document.Type.text.ToString() ? 
+                            documentTemplate.textDefault.rotation : documentTemplate.dateDefault.rotation)
+                    );
+                    layout.shiftX = GetRandomIntInRange(
+                        layoutTemplate.shiftX ??
+                        (layout.type == Document.Type.text.ToString() ? 
+                            documentTemplate.textDefault.shiftX : documentTemplate.dateDefault.shiftX)
+                    );
+                    layout.shiftY = GetRandomIntInRange(
+                        layoutTemplate.shiftY ??
+                        (layout.type == Document.Type.text.ToString() ? 
+                            documentTemplate.textDefault.shiftY : documentTemplate.dateDefault.shiftY)
+                    );
+                }
+                else
+                {
+                    layout.alignmentX = GetRandomStringFromArray(layoutTemplate.alignmentX ?? documentTemplate.imageDefault.alignmentX);
+                    layout.alignmentY = GetRandomStringFromArray(layoutTemplate.alignmentY ?? documentTemplate.imageDefault.alignmentY);
+                    layout.rotation = GetRandomIntInRange(layoutTemplate.rotation ?? documentTemplate.imageDefault.rotation);
+                    layout.value =
+                        (layoutTemplate.value ?? GetRandomValueFromFile(layoutTemplate.valuePath)) ??
+                        (documentTemplate.imageDefault.value ?? GetRandomValueFromFile(documentTemplate.imageDefault.valuePath));
+                    layout.shiftX = GetRandomIntInRange(layoutTemplate.shiftX ?? documentTemplate.imageDefault.shiftX);
+                    layout.shiftY = GetRandomIntInRange(layoutTemplate.shiftY ?? documentTemplate.imageDefault.shiftY);
+                }
+                document.layout.Add(layout);
+            }
+        }
+
+        public void AddElementToLayout(string jsonPath, Encoding encoding)
+        {
+            Document.DocumentLayout documentElements = InitDocumentLayout();
+            JsonParser.GetObject<Document.DocumentLayout>(jsonPath, encoding, out documentElements);
+            List<Document.DocumentLayout.Layout> elements = new List<Document.DocumentLayout.Layout>();
+            foreach (Document.DocumentLayout.Layout element in elements)
+            {
+                document.layout.Add(element);
+            }
+        }
+
+        public void SaveLayout(string savePath, Encoding encoding)
+        {
+            JsonParser.SaveObject(document, savePath, encoding);
+        }
+
+        private Document.DocumentLayout InitDocumentLayout()
+        {
+            Document.DocumentLayout documentLayout = new Document.DocumentLayout();
+            documentLayout.layout = new List<Document.DocumentLayout.Layout>();
+            documentLayout.size = new Document.Size();
+            return documentLayout;
+        }
+
+        private Document.DocumentLayout.FontLayout GetRandomFontFromResources(Document.DocumentTemplate.FontTemplate fontRange)
+        {
+            Document.DocumentLayout.FontLayout font = new Document.DocumentLayout.FontLayout();
+            font.fontFamily = GetRandomStringFromArray(fontRange.fontFamily);
+            font.fontColor = GetRandomStringFromArray(fontRange.fontColor);
+            font.fontSize = GetRandomIntInRange(fontRange.fontSize);
+            return font;
+        }
+
+        private string GetRandomDateInRange(string[] range)
+        {
+            DateTime startDate = GetDateFromString(range[0]);
+            DateTime endDate = GetDateFromString(range[1]);
+            int daysRange = (endDate - startDate).Days;
+            return startDate.AddDays(rand.NextDouble() * daysRange).ToString(documentTemplate.layoutDateFormat);
+        }
+
+        private DateTime GetDateFromString(string date)
+        {
+            return DateTime.ParseExact(date, documentTemplate.layoutDateFormat, dateCulture);
+        }
+
+        private string GetRandomStringFromArray(string[] arr)
+        {
+            return arr[rand.Next(0, arr.Length)];
+        }
+
+        private string GetRandomValueFromFile(string path)
+        {
+            return GetRandomStringFromArray(JsonParser.GetArrayFromJson(path, encoding));
+        }
+
+        private int GetRandomIntInRange(int[] range)
+        {
+            return rand.Next(range[0], range[1] + 1);
+        }
+
+        private int GetRandomTextRotationInRange(int[] range)
+        {
+            return GetRandomIntInRange(new int[] { range[0] / 90, range[1] / 90 }) * 90;
+        }
+    }
+}
---
+++ Layout/LayoutParser.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace Layout
+{
+    public  class LayoutParser
+    {
+        private Random rand = new Random();
+        private Document.DocumentLayout documentLayout;
+        private Bitmap targetDocument;
+        private Graphics g;
+
+        public LayoutParser(string jsonPath, Encoding encoding)
+        {
+            JsonParser.GetObject<Document.DocumentLayout>(jsonPath, encoding, out documentLayout);
+            targetDocument = new Bitmap(documentLayout.size.width, documentLayout.size.height, PixelFormat.Format32bppArgb);
+            g = Graphics.FromImage(targetDocument);
+            if (documentLayout.background != null)
+            {
+                Image image = Image.FromFile(documentLayout.background);
+                g.DrawImage(image, 0, 0, image.Width, image.Height);
+            }
+        }
+
+        public void PrintDocument(string savePath)
+        {
+            foreach (Document.DocumentLayout.Layout layout in documentLayout.layout)
+            {
+                g = Graphics.FromImage(targetDocument);
+                layout.location.left += layout.shiftX;
+                layout.location.top += layout.shiftY;
+
+                if (layout.type != Document.Type.image.ToString())
+                {
+                    DrawText(layout);
+                }
+                else
+                {
+                    DrawScaledImage(layout);
+                }
+            }
+            targetDocument = RotateDocument(targetDocument);
+            targetDocument.Save(savePath, ImageFormat.Png);
+        }
+
+        private void DrawText(Document.DocumentLayout.Layout layout)
+        {
+            StringFormat drawFormat = new StringFormat();
+            Rectangle location = new Rectangle(layout.location.left, layout.location.top, layout.location.width, layout.location.height);
+            Font font = new Font(layout.font.fontFamily, layout.font.fontSize);
+            SolidBrush drawBrush = new SolidBrush(ColorTranslator.FromHtml(layout.font.fontColor));
+            layout.location.left += layout.shiftX;
+            layout.location.top += layout.shiftY;
+            drawFormat.Alignment = GetStringAlignment((int)Enum.Parse(typeof(Document.HorizontalAlignment), layout.alignmentX, true));
+            drawFormat.LineAlignment = GetStringAlignment((int)Enum.Parse(typeof(Document.VerticalAlignment), layout.alignmentY, true));
+            switch (layout.rotation)
+            {
+                case 90:
+                    g.RotateTransform(90);
+                    g.TranslateTransform(
+                        -(layout.location.left - layout.location.top), 
+                        -(layout.location.left + layout.location.top + layout.location.width)
+                    );
+                    location = new Rectangle(location.Left, location.Top, location.Height, location.Width);
+                    break;
+                case 180:
+                    g.RotateTransform(180);
+                    g.TranslateTransform(
+                        -(layout.location.left * 2 + layout.location.width), 
+                        -(layout.location.height + layout.location.top * 2)
+                    );
+                    break;
+                case 270:
+                    g.RotateTransform(270);
+                    g.TranslateTransform(
+                        -(layout.location.left + layout.location.top + layout.location.height), 
+                        (layout.location.left - layout.location.top)
+                    );
+                    location = new Rectangle(location.Left, location.Top, location.Height, location.Width);
+                    break;
+            }
+
+            g.DrawString(layout.value, font, drawBrush, location, drawFormat);
+        }
+
+        private void DrawScaledImage(Document.DocumentLayout.Layout layout)
+        {
+            Document.HorizontalAlignment alignmentX = 
+                (Document.HorizontalAlignment)Enum.Parse(typeof(Document.HorizontalAlignment), layout.alignmentX, true);
+            Document.VerticalAlignment alignmentY = 
+                (Document.VerticalAlignment)Enum.Parse(typeof(Document.VerticalAlignment), layout.alignmentY, true);
+            Image image = Image.FromFile(layout.value);
+            Document.Location scaledLocation = new Document.Location();
+            scaledLocation.left = layout.location.left;
+            scaledLocation.top = layout.location.top;
+            if (layout.location.width > layout.location.height)
+            {
+                scaledLocation.width = (int)(image.Width * ((double)layout.location.height / image.Height));
+                scaledLocation.height = layout.location.height;
+            }
+            else
+            {
+                scaledLocation.height = (int)(image.Height * ((double)layout.location.width / image.Width));
+                scaledLocation.width = layout.location.width;
+            }
+            if ((alignmentX == Document.HorizontalAlignment.middle) && (scaledLocation.width < layout.location.width))
+            {
+                scaledLocation.left += (layout.location.width - scaledLocation.width) / 2;
+            }
+            else if ((alignmentX == Document.HorizontalAlignment.right) && (scaledLocation.width < layout.location.width))
+            {
+                scaledLocation.left += (layout.location.width - scaledLocation.width);
+            }
+            if ((alignmentY == Document.VerticalAlignment.middle) && (scaledLocation.height < layout.location.height))
+            {
+                scaledLocation.top += (layout.location.height - scaledLocation.height) / 2;
+            }
+            else if ((alignmentY == Document.VerticalAlignment.bottom) && (scaledLocation.height < layout.location.height))
+            {
+                scaledLocation.top += (layout.location.height - scaledLocation.height);
+            }
+            RotateObject(scaledLocation, layout.rotation);
+            g.DrawImage(image, scaledLocation.left, scaledLocation.top, scaledLocation.width, scaledLocation.height);
+        }
+
+        private void RotateObject(Document.Location location, float angle)
+        {
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            Point center = new Point((location.left + location.width / 2), (location.top + location.height / 2));
+            g.Transform = RotateAroundPoint(angle, center);
+        }
+        
+        public StringAlignment GetStringAlignment(int alignment)
+        {
+            switch (alignment)
+            {
+                case 0:
+                    return StringAlignment.Near;
+                case 1:
+                    return StringAlignment.Center;
+                case 2:
+                    return StringAlignment.Far;
+                default:
+                    return StringAlignment.Center;
+
+            }
+        }
+
+        private Matrix RotateAroundPoint(float angle, Point center)
+        {
+            Matrix result = new Matrix();
+            result.RotateAt(angle, center);
+            return result;
+        }
+
+        private Bitmap RotateDocument(Bitmap currentDocument)
+        {
+            Bitmap document = new Bitmap(documentLayout.size.width, documentLayout.size.height, PixelFormat.Format32bppArgb);
+            g = Graphics.FromImage(document);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            Point center = new Point(documentLayout.size.width / 2, documentLayout.size.height / 2);
+            g.Transform = RotateAroundPoint(documentLayout.rotation, center);
+            g.DrawImage(currentDocument, documentLayout.shiftX, documentLayout.shiftY, documentLayout.size.width, documentLayout.size.height);
+            return document;
+        }
+    }
+}
